766411ac6547aa6313ce87b097788833dd47822b
yonghowfung <yhfung940217@gmail.com>
2023-03-05 21:33:43 +0100

    created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+puts "Messages destroying..."
+Message.destroy_all
+puts "Messages destroyed!"
+
+puts "Users destroying..."
+User.destroy_all
+puts "Users destroyed!"
+
+puts "Channels destroying..."
+Channel.destroy_all
+puts "Channels destroyed!"
+
+puts "Channels creating..."
+channels = Channel.create!([
+  { name: "general" },
+  { name: "france" },
+  { name: "international" },
+  { name: "react" },
+  { name: "rails" },
+  { name: "ruby" },
+  { name: "other" }
+])
+puts "Channels created!"
+
+puts "Users creating..."
+users = []
+10.times do
+  user = User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6, max_length: 10)
+  )
+  users << user
+end
+puts "Users created!"
+
+puts "Messages creating..."
+100.times do
+  Message.create!(
+    user_id: users.sample.id,
+    channel_id: channels.sample.id,
+    content: Faker::Quote.famous_last_words
+  )
+end
+puts "Messages created!"
