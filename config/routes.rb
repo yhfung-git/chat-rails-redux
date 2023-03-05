d7de4434e9443298db050bf09caf1b429eb1644b
yonghowfung <yhfung940217@gmail.com>
2023-03-05 21:40:04 +0100

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :channels, only: [] do
+        resources :messages, only: [ :index, :create ]
+      end
+    end
+  end
+
+  # resources :channels, only: [ :show ]
+  # root to: 'channels#show'
 end
